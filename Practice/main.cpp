c73a6df668fb72f028ae133917a094d0cdf6f0d4
bank2014 <bank2014@naver.com>
2023-03-29 14:06:41 +0900

    a

---
+++ Practice/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using std::vector;
+
+bool binary_search(vector<int>& result, vector<int>& arr1, vector<int>& arr2)
+{
+	__m128i arr1 = _mm_load_si128()
+}
+
+int main()
+{
+	__declspec(align(32)) vector<int> arr1 = { 1, 2, 3, 4, 5 };
+	__declspec(align(32)) vector<int> arr2 = { 1, 2, 3, 4, 5 };
+	__declspec(align(32)) vector<int> result;
+
+	return 0;
+}
